437ac821d6b12711e43e4e7f74333a6439a0a651
GuillerminaS <guillerminasalinas.ok@gmail.com>
2023-03-14 10:40:20 -0300

    Data(url) works, checkBoxtes-Categories works

--- js/filterBar.js
+++ js/filterBar.js
@@ -1,86 +1,89 @@
-// This function extracts the categories from the array events, in dataEvents object.
-function getCategories() {
-    let uniqueCategory = [...new Set(dataEvents.events.map(event => event.category))];
-    return uniqueCategory;
-}
+(async function () {
+    const dataEvents = await getData();
 
-// categories list view
-const checkCategory = document.getElementById('checkCategory');
+    // This function extracts the categories from the array events, in dataEvents object.
+    function getCategories() {
+        let uniqueCategory = [...new Set(dataEvents.events.map(event => event.category))];
+        return uniqueCategory;
+    }
 
-function addCategoryCheckBox(arrayCategories) {
-    let inputCategory = " ";
+    // categories list view
+    const checkCategory = document.getElementById('checkCategory');
 
-    for (let category of arrayCategories) {
-        inputCategory += `<div>
+    function addCategoryCheckBox(arrayCategories) {
+        let inputCategory = " ";
+
+        for (let category of arrayCategories) {
+            inputCategory += `<div>
                         <label> <input type="checkbox" name="categoryCheck" class="check_box" id="${category}" value="${category}"> ${category} </label>
                         </div>`
-    }
-    return inputCategory;
-}
-
-let categoryElement = addCategoryCheckBox(getCategories());
-checkCategory.innerHTML = categoryElement;
-
-// filters:
-
-const btnSearch = document.getElementById('btnSearch');
-const searcher = document.getElementById('searcher');
-const checkBoxes = document.querySelectorAll('.check_box');
-
-let categoriesCheck = [];
-
-btnSearch.addEventListener('click', (event) => {
-
-    event.preventDefault();
-
-    checkBoxes.forEach((category) => {
-        if (category.checked) {
-            categoriesCheck.push(category.value);
         }
+        return inputCategory;
+    }
+
+    let categoryElement = addCategoryCheckBox(getCategories());
+    checkCategory.innerHTML = categoryElement;
+
+    // filters:
+
+    const btnSearch = document.getElementById('btnSearch');
+    const searcher = document.getElementById('searcher');
+    const checkBoxes = document.querySelectorAll('.check_box');
+
+    let categoriesCheck = [];
+
+    btnSearch.addEventListener('click', (event) => {
+
+        event.preventDefault();
+
+        checkBoxes.forEach((category) => {
+            if (category.checked) {
+                categoriesCheck.push(category.value);
+            }
+        });
+
+        let filteredEvents = filterEventsByNameAndDate(searcher.value, currentDate);
+
+        if (categoriesCheck.length > 0) {
+            filteredEvents = filteredEvents.filter(event => {
+                return categoriesCheck.includes(event.category)
+            });
+        };
+
+        if (filteredEvents.length == 0 || filteredEvents == null) {
+            cardContainer.innerHTML = notFoundMessage();
+        } else {
+            cardContainer.innerHTML = addCard(filteredEvents);
+        };
+
+        categoriesCheck = [];
     });
 
-    let filteredEvents = filterEventsByNameAndDate(searcher.value, currentDate);
+    //reset button
+    const btnReset = document.getElementById('btnReset');
+    btnReset.addEventListener('click', () => {
+        checkBoxes.forEach((category) => category.checked = false);
+        searcher.value = null;
+        btnSearch.click();
+    });
 
-    if (categoriesCheck.length > 0) {
-        filteredEvents = filteredEvents.filter(event => {
-            return categoriesCheck.includes(event.category)
-        });
-    };
+    //notFoundMesage
+    const titleSection = document.getElementById("section-title").textContent;
+    let urlImage = "";
 
-    if (filteredEvents.length == 0 || filteredEvents == null) {
-        cardContainer.innerHTML = notFoundMessage();
-    } else {
-        cardContainer.innerHTML = addCard(filteredEvents);
-    };
-
-    categoriesCheck = [];
-});
-
-//reset button
-const btnReset = document.getElementById('btnReset');
-btnReset.addEventListener('click',()=> {
-    checkBoxes.forEach((category) => category.checked = false);
-    searcher.value = null;
-    btnSearch.click();
-});
-
-//notFoundMesage
-const titleSection = document.getElementById("section-title").textContent;
-let urlImage = "";
-
-function getUrlImage(title){
-    if (title == "HOME"){
-        return urlImage = "./assets/notFound.png";
-    } else {
-        return urlImage = "../assets/notFound.png";
+    function getUrlImage(title) {
+        if (title == "HOME") {
+            return urlImage = "./assets/notFound.png";
+        } else {
+            return urlImage = "../assets/notFound.png";
+        }
     }
-}
 
-function notFoundMessage() {
+    function notFoundMessage() {
 
-    url = getUrlImage(titleSection);
+        url = getUrlImage(titleSection);
 
-    return divMessage = `<div class="container ps-2 d-flex justify-content-center flex-wrap align-items-center">
+        return divMessage = `<div class="container ps-2 d-flex justify-content-center flex-wrap align-items-center">
         <div class="img-notFound">
             <img src=${url} alt="message: not found">
         </div>
@@ -90,4 +93,5 @@ function notFoundMessage() {
             <p>Please, try with other categories, name, or check other web sections.</p>
         </div>
     </div>`
-};
+    };
+})();
